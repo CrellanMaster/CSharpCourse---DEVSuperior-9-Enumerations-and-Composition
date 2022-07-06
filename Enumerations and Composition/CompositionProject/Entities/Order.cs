649f5d9ad58879af20abcd4c4ff354edd0a9b1e4
CrellanMaster <devsandrounity@gmail.com>
2022-07-06 09:21:45 -0300

    Composition Project Solved Solo

---
+++ Enumerations and Composition/CompositionProject/Entities/Order.cs
@@ -0,0 +1,66 @@
+﻿using CompositionProject.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompositionProject.Entities
+{
+    internal class Order
+    {
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+        public List<OrderItem> Items { get; set; } = new List<OrderItem>();
+        public Client Client { get; set; }
+        public double TotalValue { get; set; }
+
+        public Order()
+        {
+
+        }
+        public Order(DateTime moment, OrderStatus status, Client client)
+        {
+            Moment = moment;
+            Status = status;
+            Client = client;
+        }
+
+        public void AddItem(OrderItem item)
+        {
+            Items.Add(item);
+        }
+
+        public void RemoveItem(OrderItem item)
+        {
+            Items.Remove(item);
+        }
+
+        public double Total()
+        {
+
+            foreach (OrderItem order in Items)
+            {
+                TotalValue += order.SubTotal();
+            }
+            return TotalValue;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Order moment: {Moment}");
+            sb.AppendLine($"Order Status: {Status}");
+            sb.AppendLine($"Client: {Client.Name} ({Client.BirthDate}) - {Client.Email}");
+            sb.AppendLine("Order Items: ");
+
+            foreach (OrderItem order in Items)
+            {
+                sb.AppendLine(order.ToString());
+            }
+            sb.Append($"Total Price: ${Total().ToString("F2", CultureInfo.InvariantCulture)}");
+            return sb.ToString();
+        }
+    }
+}
---
+++ Enumerations and Composition/CompositionProject/Program.cs
@@ -0,0 +1,62 @@
+﻿using CompositionProject.Entities.Enums;
+using System.Globalization;
+using CompositionProject.Entities;
+
+namespace CompositionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter cliente data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Birth date (MM/DD/YYYY):");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+
+            Client client = new Client(
+                name,
+                email,
+                birthDate
+                );
+
+            Console.WriteLine("Enter order data: ");
+            Console.WriteLine($"Status: {OrderStatus.Processing}");
+            Console.Write("How many items to this order? ");
+            int number = int.Parse(Console.ReadLine());
+
+            Order order = new Order(
+                            DateTime.Now,
+                            OrderStatus.Processing,
+                            client
+                            );
+
+            for (int i = 1; i <= number; i++)
+            {
+                Console.WriteLine($"Enter #{i} item data: ");
+                Console.Write("Product name: ");
+                string productName = Console.ReadLine();
+                Console.Write("Product Price: ");
+                double productPrice = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Quantity: ");
+                int quantity = int.Parse(Console.ReadLine());
+
+                Product product = new Product(
+                    productName,
+                    productPrice
+                    );
+                OrderItem orderItem = new OrderItem(
+                    quantity,
+                    productPrice,
+                    product
+                    );
+                order.AddItem(orderItem);
+            }
+
+            Console.WriteLine("ORDER SUMARY: ");
+            Console.WriteLine(order.ToString());
+        }
+    }
+}
